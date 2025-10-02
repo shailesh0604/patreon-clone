7ec871daab2c38b656e39b1c770076d1ce7b4044
Shailesh Bind <Shaileshbind39@gmail.com>
2025-10-02 21:35:29 +0530

    updation in 404 page

--- Components/NotFound404.js
+++ Components/NotFound404.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Particles from './Particle';
 import Navbar from '@/Components/Navbar';
+import Link from 'next/link';
 
 const NotFound404 = () => {
     return <>
@@ -17,7 +18,21 @@ const NotFound404 = () => {
                 disableRotation={false}
             />
 
-            <div className="text-white text-4xl z-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">404</div>
+            <div className="text-white text-4xl z-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%]">
+                <div className="title-404 text-center">
+                    404
+                </div>
+                <div className="subtitle-404 text-center">
+                    Page Not Found.
+                </div>
+                <div className="desc-404 text-center">
+                    It seems the page you're looking for doesn't exist.<br /> Let's get you back on track.
+                </div>
+                <div className="flex justify-center">
+                    <Link className='btn-return' href={"/"}>Return to home page</Link>
+                </div>
+
+            </div>
         </div>
     </>
 }
